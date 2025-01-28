05285a568541e0556db88289b27ad0e4e89c7309
Mihai Tudose <mihaitudose.eu@gmail.com>
2025-01-28 19:12:02 +0200

    root tab bar

---
+++ MovieBrowser/Sources/Presentation/Root/RootTabBarView.swift
@@ -0,0 +1,36 @@
+//
+//  RootTabBarView.swift
+//  MovieBrowser
+//
+//  Created by Mihai Tudose on 28.01.2025.
+//
+
+import SwiftUI
+
+struct RootTabBarView: View {
+    var body: some View {
+        TabView {
+            Text("First Tab")
+                .tabItem {
+                    Image(systemName: "popcorn.circle")
+                }
+            
+            Text("Second Tab")
+                .tabItem {
+                    Image(systemName: "ticket")
+                }
+            
+            Text("Third Tab")
+                .tabItem {
+                    Image(systemName: "bookmark")
+                }
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootTabBarView()
+    }
+}
+
