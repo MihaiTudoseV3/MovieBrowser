356856081780f42e3a4f33e46504a6aa0319519c
Mihai Tudose <mihaitudose.eu@gmail.com>
2025-01-28 19:10:20 +0200

    new project

---
+++ MovieBrowser/MovieBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBrowserApp.swift
+//  MovieBrowser
+//
+//  Created by Mihai Tudose on 28.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieBrowserTests/MovieBrowserTests.swift
@@ -0,0 +1,16 @@
+//
+//  MovieBrowserTests.swift
+//  MovieBrowserTests
+//
+//  Created by Mihai Tudose on 28.01.2025.
+//
+
+import Testing
+
+struct MovieBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
